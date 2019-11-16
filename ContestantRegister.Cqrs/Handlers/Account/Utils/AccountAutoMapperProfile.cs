63566161b08042bd91f05abdbdf04fcce081469f
Denis <den.tsvettsih@yandex.ru>
2019-11-16 13:22:42 +0700

    trim user names on registration

--- ContestantRegister.Cqrs/Handlers/Account/Utils/AccountAutoMapperProfile.cs
+++ ContestantRegister.Cqrs/Handlers/Account/Utils/AccountAutoMapperProfile.cs
@@ -8,7 +8,13 @@ namespace ContestantRegister.Cqrs.Features.Frontend.Account.Utils
     {
         public AccountAutoMapperProfile()
         {
-            CreateMap<RegisterViewModel, ApplicationUser>();
+            CreateMap<RegisterViewModel, ApplicationUser>()
+                .ForMember(x => x.Surname, opt => opt.MapFrom(y => y.Surname.Trim()))
+                .ForMember(x => x.Name, opt => opt.MapFrom(y => y.Name.Trim()))
+                .ForMember(x => x.Patronymic, opt => opt.MapFrom(y => y.Patronymic.Trim()))
+                .ForMember(x => x.FirstName, opt => opt.MapFrom(y => y.FirstName.Trim()))
+                .ForMember(x => x.LastName, opt => opt.MapFrom(y => y.LastName.Trim()))
+                ;
         }
     }
 }
